bad0c610ca841779087776a0e8810ff000642ed8
leoromero97 <leonardogerbacio@gmail.com>
2020-09-21 12:23:57 -0300

    Se agregaron 3 imagenes a la seccion de clientes. Se modificaron iconos de servicios. Se ajustaron tamaños de texto para la vista movil

--- script.js
+++ script.js
@@ -1,12 +1,38 @@
 let burger = document.querySelector(".burger");
 let topbar = document.querySelector(".links");
 let inicio = document.getElementById("homeI");
-let sobreMi = document.getElementById("aboutI");
-let contact = document.getElementById("contactI");
-let menu = document.getElementById("menuI");
+let about = document.getElementById("aboutI");
+let services = document.getElementById("servicesI");
+let clients = document.getElementById("clientsI");
+let footer = document.getElementById("footerI");
 
 burger.addEventListener("click", () => {
   topbar.classList.toggle("sidebar");
   //Animacion del menu hamburguesa, pasa a ser una X
+  burger.classList.toggle("toggle");
+});
+
+inicio.addEventListener("click", ()=>{
+  topbar.classList.toggle("sidebar");
+  burger.classList.toggle("toggle");
+});
+
+about.addEventListener("click", ()=>{
+  topbar.classList.toggle("sidebar");
+  burger.classList.toggle("toggle");
+});
+
+services.addEventListener("click", ()=>{
+  topbar.classList.toggle("sidebar");
+  burger.classList.toggle("toggle");
+});
+
+clients.addEventListener("click", ()=>{
+  topbar.classList.toggle("sidebar");
+  burger.classList.toggle("toggle");
+});
+
+footer.addEventListener("click", ()=>{
+  topbar.classList.toggle("sidebar");
   burger.classList.toggle("toggle");
 });
