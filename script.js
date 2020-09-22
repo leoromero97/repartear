5e63a16b349648034728e1b24d3540b75db4ec17
leoromero97 <leonardogerbacio@gmail.com>
2020-09-22 01:06:47 -0300

    Se cambiaron imagenes en la pagina. Se agregaron imagenes y links de clientes. Se creo un modal ubicado en el pie de pagina

--- script.js
+++ script.js
@@ -50,16 +50,47 @@ window.onscroll = function () {
   }
 }
 
-
-
 window.addEventListener("scroll", () => {
   let currentHeight = window.scrollY; //parte superior de la pantalla
-  let homePositionY = home.scrollHeight = +500;
+  let homePositionY = home.scrollHeight = +400;
   if (currentHeight >= homePositionY) {
     document.querySelector(".containerbuttonWPP").classList.add("showButtonWPP");
     document.querySelector(".up").classList.remove("ocultarContenido");
   } else {
     document.querySelector(".containerbuttonWPP").classList.remove("showButtonWPP");
     document.querySelector(".up").classList.add("ocultarContenido");
+  }
+});
+
+//A partir de esta linea, empieza el codigo para controlar el modal
+let cerrar = document.querySelectorAll(".close")[0];
+let abrir = document.querySelectorAll(".openModal")[0];
+let modal = document.querySelectorAll(".modal")[0];
+let modalContainer = document.querySelectorAll(".modalContainer")[0];
+
+abrir.addEventListener("click", function (event) {
+  event.preventDefault();
+  modalContainer.style.opacity = "1";
+  modalContainer.style.visibility = "visible";
+  modal.classList.toggle("modalClose");
+});
+
+cerrar.addEventListener("click", function () {
+  modal.classList.toggle("modalClose");
+  //con el set timeout creo una funcion para que a 6segundos pasen los estilos que especifico antes.
+  setTimeout(function () {
+    modalContainer.style.opacity = "0";
+    modalContainer.style.visibility = "hidden";
+  }, 600);
+});
+//Con este script hago click en cualquier lado del modal y se cierra
+window.addEventListener("click", function (event) {
+  if (event.target == modalContainer) {
+    modal.classList.toggle("modalClose");
+
+    setTimeout(function () {
+      modalContainer.style.opacity = "0";
+      modalContainer.style.visibility = "hidden";
+    }, 600);
   }
 });
